899ca0fbfd0b9a065434864c3b4d617f9225b113
pilkyu <anothel1@naver.com>
2022-05-07 23:10:41 +0900

    [Bronze V] Title: 새싹, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,14 @@
+from sys import stdin, stdout
+
+
+def solve():
+  stdout.write("         ,r\'\"7\n")
+  stdout.write("r`-_   ,'  ,/\n")
+  stdout.write(" \. \". L_r'\n")
+  stdout.write("   `~\/\n")
+  stdout.write("      |\n")
+  stdout.write("      |\n")
+
+
+if __name__ == "__main__":
+    solve()
