9fafae16747d0f5de9b59286ab0cdf89e02a1cf0
pilkyu <anothel1@naver.com>
2022-05-29 08:31:52 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 172 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.py
@@ -0,0 +1,11 @@
+from sys import stdin, stdout
+
+
+def solve():
+  for _ in range(int(stdin.readline().rstrip())):
+    stdout.write("a")
+  stdout.write("\n")
+
+
+if __name__ == "__main__":
+    solve()
