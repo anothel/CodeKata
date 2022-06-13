7f02aa2b678a7e573cf70257d2a9e4c710c99d56
pilkyu <anothel1@naver.com>
2022-06-14 08:00:47 +0900

    [Bronze IV] Title: 모음의 개수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -0,0 +1,19 @@
+from sys import stdin, stdout
+
+
+def solution():
+  words: list = ["a", "e", "i", "o", "u"]
+  while True:
+    s: str = stdin.readline().rstrip().lower()
+    if s == "#":
+      break
+
+    count: int = 0
+    for i in s:
+      if i in words:
+        count += 1
+    stdout.write("%d\n" % count)
+
+
+if __name__ == "__main__":
+  solution()
