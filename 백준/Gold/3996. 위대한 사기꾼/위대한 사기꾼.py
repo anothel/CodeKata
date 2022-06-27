12fa62c06c94d8d3f17e71c263b3edc76b3aa862
pilkyu <anothel1@naver.com>
2022-06-27 15:03:33 +0900

    [Gold IV] Title: 위대한 사기꾼, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/3996. 위대한 사기꾼/위대한 사기꾼.py
+++ 백준/Gold/3996. 위대한 사기꾼/위대한 사기꾼.py
@@ -1,83 +1,32 @@
-from sys import stdin
-
-
-def getNormalNumber(n, k):
-  sReturn = list()
-
-  while n:
-    sReturn.insert(0, n % k)
-    n //= k
-
-  return sReturn
-
-# def getMinusNumber(n, k):
-#   sReturn = ''
-
-#   while k <= abs(n):
-#     sReturn += str(n % k)
-
-#     if n < 0:
-#       n = n // k * -1
-#     else:
-#       n = int(n / k) * -1
-
-#   n *= -1
-#   if 0 < n:
-#     n *= -1
-#     sReturn += str(n % k)
-#     n = int(n / k) - 1
-
-#   sReturn += str(n * -1)
-
-#   return sReturn[::-1]
+from sys import stdin, stdout
 
 
 def main():
-  n, k = map(int, stdin.readline().split())
-  
-  # print(getNormalNumber(n, k))
-  
-  
-  sReturn = list()
+    n, k = map(int, stdin.readline().split())
 
-  while n:
-    sReturn.insert(0, n % (k*k))
-    n //= k*k
+    sReturn: list = list()
 
-  count = 0
-  f = 0
+    while n:
+        sReturn.insert(0, n % (k * k))
+        n //= k * k
 
-  # print(sReturn)
+    count: int = 0
+    f: int = 0
 
-  for i in sReturn:
-    if f == 0:
-      if k <= i:
-        count += k
-        f = 1
-      else:
-        count += i
-    count *= k
+    for i in sReturn:
+        if f == 0:
+            if k <= i:
+                count += k
+                f = 1
+            else:
+                count += i
+        count *= k
 
-  count //= k
-  if(f == 0):
-    count += 1
+    count //= k
+    if (f == 0):
+        count += 1
 
-  print(count)
-
-  # for i in range(n + 1):
-  #   normal = getNormalNumber(i, k*k)
-  #   # print(normal)
-  #   if len(normal) < 2:
-  #     # print(normal)
-  #     count += 1
-  #     continue
-  #   elif len(normal) % 2 == 0:
-  #     continue
-  #   else:
-  #     if normal[1] == 0:
-  #       count += 1
-
-  # print(count)
+    stdout.write("%d\n" % count)
 
 
 if __name__ == "__main__":
