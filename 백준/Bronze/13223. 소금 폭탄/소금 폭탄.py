dcc7fc32fa5e814ff2822e5f36734f5dc14f5cf0
pilkyu <anothel1@naver.com>
2022-07-18 08:52:43 +0900

    [Bronze III] Title: 소금 폭탄, Time: 92 ms, Memory: 35348 KB -BaekjoonHub

---
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.py
@@ -0,0 +1,27 @@
+from sys import stdin, stdout
+import datetime
+
+
+def solution():
+    hh1, mm1, ss1 = map(int, stdin.readline().rstrip().split(":"))
+    hh2, mm2, ss2 = map(int, stdin.readline().rstrip().split(":"))
+
+    if hh1 > hh2 or (hh1 <= hh2 and mm1 > mm2) or (hh1 <= hh2 and mm1 <= mm2
+                                                   and ss1 > ss2):
+        dt = datetime.datetime(2022, 7, 19, hh2, mm2, ss2) - datetime.datetime(
+            2022, 7, 18, hh1, mm1, ss1)
+        stdout.write("%02d:%02d:%02d\n" % (dt.seconds // 3600,
+                                           (dt.seconds % 3600) // 60,
+                                           (dt.seconds % 60) % 60))
+    elif hh1 == hh2 and mm1 == mm2 and ss1 == ss2:
+        stdout.write("24:00:00")
+    else:
+        dt = datetime.datetime(2022, 7, 18, hh2, mm2, ss2) - datetime.datetime(
+            2022, 7, 18, hh1, mm1, ss1)
+        stdout.write("%02d:%02d:%02d\n" % (dt.seconds // 3600,
+                                           (dt.seconds % 3600) // 60,
+                                           (dt.seconds % 60) % 60))
+
+
+if __name__ == "__main__":
+    solution()
