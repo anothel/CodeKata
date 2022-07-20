c8f9616fe6c819b5f02b57dc223a701efe124ca6
pilkyu <anothel1@naver.com>
2022-07-21 08:01:40 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] 골뱅이 찍기 - ㅂ - 23808 
+
+[문제 링크](https://www.acmicpc.net/problem/23808) 
+
+### 성능 요약
+
+메모리: 30840 KB, 시간: 92 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.py
@@ -0,0 +1,46 @@
+from sys import stdin, stdout
+
+
+def print1(n: int):
+    for _ in range(n):
+        for _ in range(n):
+            stdout.write("@")
+        for _ in range(3 * n):
+            stdout.write("@")
+        for _ in range(n):
+            stdout.write("@")
+        stdout.write("\n")
+
+
+def print2(n: int):
+    for _ in range(2 * n):
+        for _ in range(n):
+            stdout.write("@")
+        for _ in range(3 * n):
+            stdout.write(" ")
+        for _ in range(n):
+            stdout.write("@")
+        stdout.write("\n")
+
+
+def print3(n: int):
+    for _ in range(1 * n):
+        for _ in range(n):
+            stdout.write("@")
+        for _ in range(3 * n):
+            stdout.write(" ")
+        for _ in range(n):
+            stdout.write("@")
+        stdout.write("\n")
+
+
+def solve():
+    n: int = int(stdin.readline().rstrip())
+    print2(n)
+    print1(n)
+    print3(n)
+    print1(n)
+
+
+if __name__ == "__main__":
+    solve()
