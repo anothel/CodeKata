fb5055df0eae799985f798ca17431b0cb347d805
pilkyu <anothel1@naver.com>
2022-07-21 08:06:07 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] 골뱅이 찍기 - ㅌ - 23811 
+
+[문제 링크](https://www.acmicpc.net/problem/23811) 
+
+### 성능 요약
+
+메모리: 30840 KB, 시간: 84 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.py
@@ -0,0 +1,32 @@
+from sys import stdin, stdout
+
+
+def print1(n: int):
+    for _ in range(n):
+        for _ in range(n):
+            stdout.write("@")
+        for _ in range(3 * n):
+            stdout.write("@")
+        for _ in range(n):
+            stdout.write("@")
+        stdout.write("\n")
+
+
+def print2(n: int):
+    for _ in range(1 * n):
+        for _ in range(n):
+            stdout.write("@")
+        stdout.write("\n")
+
+
+def solve():
+    n: int = int(stdin.readline().rstrip())
+    print1(n)
+    print2(n)
+    print1(n)
+    print2(n)
+    print1(n)
+
+
+if __name__ == "__main__":
+    solve()
