9562c89ce53cd1a8091ac00e6891151f7a80754a
pilkyu <anothel1@naver.com>
2022-07-31 22:54:32 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 4.34 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "김서방은 ";
+    int i;
+    for(i = 0; i < seoul.size(); i++) {
+        if(seoul.at(i) == "Kim") break;
+    }
+    answer += to_string(i);
+    answer += "에 있다";
+    return answer;
+}
