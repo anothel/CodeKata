fc9f6deb33ea5993367364873a7d0d4570b31aa1
pilkyu <anothel1@naver.com>
2022-07-31 23:01:39 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;    
+    for(int i = 0; i < 10; i++) {
+        if(find(numbers.begin(), numbers.end(), i) == numbers.end()) {
+            answer += i;             
+        }
+    }
+    return answer;
+}
