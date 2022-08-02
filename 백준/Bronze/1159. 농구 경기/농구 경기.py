1ffab187acca317aa70ef59512de16445046d541
pilkyu <anothel1@naver.com>
2022-08-02 09:35:07 +0900

    [Bronze II] Title: 농구 경기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.py
+++ 백준/Bronze/1159. 농구 경기/농구 경기.py
@@ -2,37 +2,10 @@ from sys import stdin, stdout
 
 
 def solve():
-    alpha: dict = {
-        'a': 0,
-        'b': 0,
-        'c': 0,
-        'd': 0,
-        'e': 0,
-        'f': 0,
-        'g': 0,
-        'h': 0,
-        'i': 0,
-        'j': 0,
-        'k': 0,
-        'l': 0,
-        'm': 0,
-        'n': 0,
-        'o': 0,
-        'p': 0,
-        'q': 0,
-        'r': 0,
-        's': 0,
-        't': 0,
-        'u': 0,
-        'v': 0,
-        'w': 0,
-        'x': 0,
-        'y': 0,
-        'z': 0
-    }
+    alpha: dict = {chr(i): 0 for i in range(97, 123)}
+
     for _ in range(int(stdin.readline().rstrip())):
-        name: list = list(stdin.readline().rstrip())
-        alpha[name[0]] += 1
+        alpha[list(stdin.readline().rstrip())[0]] += 1
     checkPass: bool = False
     for check in alpha:
         if alpha[check] >= 5:
