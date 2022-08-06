8be29fd45bce8c33018de4900bcd231754f7653c
pilkyu <anothel1@naver.com>
2022-08-06 18:04:13 +0900

    [Bronze III] Title: 2루수 이름이 뭐야, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/17350. 2루수 이름이 뭐야/2루수 이름이 뭐야.py
+++ 백준/Bronze/17350. 2루수 이름이 뭐야/2루수 이름이 뭐야.py
@@ -4,8 +4,7 @@ from sys import stdin, stdout
 def solution():
     found: bool = False
     for _ in range(int(stdin.readline().rstrip())):
-        player: str = stdin.readline().rstrip()
-        if 'anj' == player:
+        if 'anj' == stdin.readline().rstrip():
             found = True
     stdout.write("뭐야;\n" if found else "뭐야?")
 
