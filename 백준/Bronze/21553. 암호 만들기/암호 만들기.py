f534c4bd2d0d2a593d9033edb339e8b7b15d7f3e
pilkyu <anothel1@naver.com>
2022-08-10 08:23:20 +0900

    [Bronze III] Title: 암호 만들기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.py
@@ -0,0 +1,10 @@
+from sys import stdin, stdout
+
+
+def solution():
+    a: int = int(stdin.readline().rstrip())
+    stdout.write("%d\n" % int(stdin.readline().rstrip()))
+
+
+if __name__ == "__main__":
+    solution()
