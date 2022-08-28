b5d3b1493c028a5f4287787c0eb97584fc92dc71
pilkyu <anothel1@naver.com>
2022-08-28 16:49:46 +0900

    [Bronze III] Title: Dyslexia, Time: 124 ms, Memory: 124304 KB -BaekjoonHub

---
+++ 백준/Bronze/8371. Dyslexia/Dyslexia.py
@@ -0,0 +1,19 @@
+from sys import stdin, stdout
+
+
+def solution():
+    _ = int(stdin.readline().rstrip())
+    mine: list = list(stdin.readline().rstrip())
+    cor: list = list(stdin.readline().rstrip())
+
+    answer: int = 0
+
+    for i in range(len(mine)):
+        if mine[i] != cor[i]:
+            answer += 1
+
+    stdout.write("%d\n" % answer)
+
+
+if __name__ == "__main__":
+    solution()
