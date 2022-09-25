9a6ae9545c2966662feb82e216b13088adf72362
pilkyu <anothel1@naver.com>
2022-09-25 11:50:14 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 156 ms, Memory: 139076 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,20 @@
+from sys import stdin, stdout
+import itertools
+
+
+def solution():
+    nums: list = list()
+    for _ in range(9):
+        nums.append(int(stdin.readline().rstrip()))
+    nums: list = list(itertools.permutations(nums, 7))
+
+    for i in nums:
+        if sum(i) == 100:
+            lSorted: list = sorted(i)
+            for j in lSorted:
+                stdout.write("%d\n" % j)
+            break
+
+
+if __name__ == "__main__":
+    solution()
