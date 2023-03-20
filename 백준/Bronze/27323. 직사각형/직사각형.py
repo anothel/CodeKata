9efffa4699499cfeea9b67d2c03be129e7f86ad4
pilkyu <anothel1@naver.com>
2023-03-21 08:32:06 +0900

    [Bronze V] Title: 직사각형, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.py
@@ -0,0 +1,11 @@
+from sys import stdin, stdout
+
+
+def solution():
+    a: int = int(stdin.readline().rstrip())
+    b: int = int(stdin.readline().rstrip())
+
+    stdout.write("%d\n" % (a * b))
+
+if __name__ == "__main__":
+    solution()
