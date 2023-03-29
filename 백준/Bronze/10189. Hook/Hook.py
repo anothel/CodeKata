54d23610f6d182a2fe41b6125eff5e39d05d38e4
pilkyu <anothel1@naver.com>
2023-03-29 16:32:15 +0900

    [Bronze V] Title: Hook, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.py
@@ -0,0 +1,13 @@
+from sys import stdin, stdout
+
+
+def solution():
+
+    stdout.write("#  # #### #### #  #\n")
+    stdout.write("#### #  # #  # # #\n")
+    stdout.write("#### #  # #  # # #\n")
+    stdout.write("#  # #### #### #  #\n")
+
+
+if __name__ == "__main__":
+    solution()
