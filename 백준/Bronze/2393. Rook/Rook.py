d0e3ae250109215c2fbef17860a249ef8bc6e28c
pilkyu <anothel1@naver.com>
2023-05-15 10:22:42 +0900

    [Bronze V] Title: Rook, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.py
@@ -0,0 +1,22 @@
+from sys import stdin
+
+
+def solution():
+
+    print("  ___  ___  ___")
+    print("  | |__| |__| |")
+    print("  |           |")
+    print("   \_________/")
+    print("    \_______/")
+    print("     |     |")
+    print("     |     |")
+    print("     |     |")
+    print("     |     |")
+    print("     |_____|")
+    print("  __/       \__")
+    print(" /             \\")
+    print("/_______________\\")
+
+
+if __name__ == "__main__":
+    solution()
