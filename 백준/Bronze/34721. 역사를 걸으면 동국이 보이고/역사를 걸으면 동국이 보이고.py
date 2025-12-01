13dbd4a8956557b861cef74e766db03c653fcaf9
pilkyu <anothel1@naver.com>
2025-12-01 18:08:05 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.py
@@ -0,0 +1,11 @@
+from sys import stdin
+
+
+def solution(N):
+    for _ in range(N):
+        print("I love DGU")
+
+
+if __name__ == "__main__":
+    N = int(stdin.readline().strip())
+    solution(N)
