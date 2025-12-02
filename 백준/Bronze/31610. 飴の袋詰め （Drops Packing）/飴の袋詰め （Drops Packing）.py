a1a13a1218c64628fabb41a9c12d0ce6f2e35694
pilkyu <anothel1@naver.com>
2025-12-02 21:39:09 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+
+def solution(a, b, c):
+    print(a*b+c)
+
+
+if __name__ == "__main__":
+    a = (int((stdin.readline().strip())))
+    b = (int((stdin.readline().strip())))
+    c = (int((stdin.readline().strip())))
+
+    solution(a, b, c)
