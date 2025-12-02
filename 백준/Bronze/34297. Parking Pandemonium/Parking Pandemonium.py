4b1de25de7dbb127492a915dd9989543eb8d3b6b
pilkyu <anothel1@naver.com>
2025-12-02 22:07:20 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+
+
+
+def solution(m,k,c):
+
+    print(m *c)
+
+
+if __name__ == "__main__":
+    m = int(stdin.readline().strip())
+    k = int(stdin.readline().strip())
+    c = int(stdin.readline().strip())
+
+    solution(m,k,c)
