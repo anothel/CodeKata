82a2ed4079899083dc750c32c7ab34dcfa94d954
pilkyu <anothel1@naver.com>
2025-12-02 22:02:36 +0900

    [Bronze V] Title: M-Climb, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+
+
+
+def solution(m,n,y):
+
+    print(m*n)
+
+
+if __name__ == "__main__":
+    m = int(stdin.readline().strip())
+    n = int(stdin.readline().strip())
+    y = int(stdin.readline().strip())
+
+    solution(m,n,y)
