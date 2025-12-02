8df15464241f921c2d7e4fc5bcaf3cf893ae12f1
pilkyu <anothel1@naver.com>
2025-12-02 22:13:03 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+
+
+
+def solution(t,v):
+
+    print(t*v)
+
+
+if __name__ == "__main__":
+    t = int(stdin.readline().strip())
+    v = int(stdin.readline().strip())
+
+    solution(t,v)
