fe289d9c3d1e4242f04ca3a7c98a74cc9b8562df
pilkyu <anothel1@naver.com>
2025-12-02 21:31:54 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,28 @@
+from sys import stdin
+
+
+a = {
+    1: (11, 'A B C D E F G H J L M'),
+    2: (9, 'A C E F G H I L M'),
+    3: (9, 'A C E F G H I L M'),
+    4: (9, 'A B C E F G H L M'),
+    5: (8, 'A C E F G H L M'),
+    6: (8, 'A C E F G H L M'),
+    7: (8, 'A C E F G H L M'),
+    8: (8, 'A C E F G H L M'),
+    9: (8, 'A C E F G H L M'),
+    10: (8, 'A B C F G H L M')
+}
+
+
+def solution(n):
+    num, s = a[n]
+
+    print(num)
+    print(s)
+
+
+if __name__ == "__main__":
+    n = int(stdin.readline().strip())
+
+    solution(n)
