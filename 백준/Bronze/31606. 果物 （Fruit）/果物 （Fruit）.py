ab1d60c1599489a00747da98de08c1f13ffd81a8
pilkyu <anothel1@naver.com>
2025-12-02 21:40:29 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+
+def solution(x, y):
+    print(x+y+3)
+
+
+if __name__ == "__main__":
+    x = (int((stdin.readline().strip())))
+    y = (int((stdin.readline().strip())))
+
+    solution(x, y)
