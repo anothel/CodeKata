c5440ca5720f904a85f1c0056f41128e51229e95
pilkyu <anothel1@naver.com>
2025-12-02 21:55:54 +0900

    [Bronze V] Title: E-Days Ore Cart Pull, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+
+
+
+def solution(N,C,P):
+    print(2*N)
+
+
+if __name__ == "__main__":
+    N = int(stdin.readline().strip())
+    C = int(stdin.readline().strip())
+    P = int(stdin.readline().strip())
+
+    solution(N,C,P)
