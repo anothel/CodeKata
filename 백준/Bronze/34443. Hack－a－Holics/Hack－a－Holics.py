efd48487570d31ea162dcd8c69525dc95079b0a8
pilkyu <anothel1@naver.com>
2025-12-02 21:57:43 +0900

    [Bronze V] Title: Hack-a-Holics, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34443. Hack－a－Holics/Hack－a－Holics.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+
+
+
+def solution(N,C,P):
+    print(N*P)
+
+
+if __name__ == "__main__":
+    N = int(stdin.readline().strip())
+    C = int(stdin.readline().strip())
+    P = int(stdin.readline().strip())
+
+    solution(N,C,P)
